e6b7638e4818c331facc6491e9479c48fb38bf97
jasonM46 <70234989+jasonM46@users.noreply.github.com>
2023-08-24 18:06:31 -0400

    commiting use effect....

--- front-end/app/components/navbar.js
+++ front-end/app/components/navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {LoginContext, useLoginContext} from '../context/loginContext';
 import Link from "next/link";
 import SignInPopup from "./signInPopUp";
