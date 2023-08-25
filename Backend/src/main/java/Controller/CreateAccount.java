077de52cf03c8025c02b5e02c893ff01147ad159
jasonM46 <70234989+jasonM46@users.noreply.github.com>
2023-08-24 23:16:41 -0400

    Create account servlet

---
+++ Backend/src/main/java/Controller/CreateAccount.java
@@ -0,0 +1,64 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+
+import Model.Account;
+
+/**
+ * Servlet implementation class CreateAccount
+ */
+@WebServlet("/CreateAccount")
+public class CreateAccount extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateAccount() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			JsonObject requestJson = Utilities.getJsonBody(request);
+			
+			Account newUser = new Account();
+			
+			newUser.setUserName(requestJson.get("username").getAsString());
+			newUser.setFirstName(requestJson.get("firstName").getAsString());
+			newUser.setLastName(requestJson.get("lastName").getAsString());
+			newUser.setPhoneNumber(requestJson.get("phoneNumber").getAsString());
+			newUser.setProvince(requestJson.get("province").getAsString());
+			newUser.setCountry(requestJson.get("Country").getAsString());
+			newUser.setBillingAddress(requestJson.get("billingAddress").getAsString());
+			newUser.setAddress(requestJson.get("address").getAsString());
+			newUser.setEmail(requestJson.get("email").getAsString());
+			newUser.setPostalCode(requestJson.get("postalCode").getAsString());
+			
+			
+			String password = requestJson.get("password").getAsString();
+			String premissions = requestJson.get("premissions").getAsString();
+			
+			new DataAccsessMySQL().addUser(newUser, password, premissions);
+		} catch (Exception e) {
+			response.getWriter().print("somthing went wrong");
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
