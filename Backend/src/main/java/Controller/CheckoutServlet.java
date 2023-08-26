49537918d8e35491041f1f5b820dc7d3cb9b92ea
jasonM46 <70234989+jasonM46@users.noreply.github.com>
2023-08-26 17:01:55 -0400

    removing java mail imports

--- Backend/src/main/java/Controller/CheckoutServlet.java
+++ Backend/src/main/java/Controller/CheckoutServlet.java
@@ -1,17 +1,10 @@
 package Controller;
 
 import java.io.IOException;
-import javax.mail.Authenticator;
 
 import java.util.Properties;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
